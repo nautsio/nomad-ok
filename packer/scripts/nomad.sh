0a26d6e0918cc1644eefb76b2b0582d513951f51
Bastiaan Bakker <bbakker@xebia.com>
2015-12-31 10:47:24 +0100

    Download Nomad binary

--- packer/scripts/nomad.sh
+++ packer/scripts/nomad.sh
@@ -2,9 +2,12 @@
 # Quit on errors.
 set -e
 
+# Download and install Nomad binary
+curl https://releases.hashicorp.com/nomad/0.2.3/nomad_0.2.3_linux_amd64.zip > /tmp/nomad.zip
+sudo unzip /tmp/nomad.zip -d /usr/bin
+
 # Move the Nomad files.
 sudo mv /tmp/etc/nomad.d /etc/nomad.d
-sudo mv /tmp/usr/bin/nomad /usr/bin/nomad
 
 # Set ownership and rights on Nomad directories.
 sudo install -o root -g root -m 755 -d /etc/nomad.d /var/local/nomad
