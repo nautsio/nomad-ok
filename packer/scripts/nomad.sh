e2a698b32d50e3501546a0557b6d5991d490a14b
Bastiaan Bakker <bbakker@xebia.com>
2016-01-01 23:35:51 +0100

    enable nomad service don't need sudo in provision script

--- packer/scripts/nomad.sh
+++ packer/scripts/nomad.sh
@@ -4,14 +4,16 @@ set -e
 
 # Download and install Nomad binary
 curl https://releases.hashicorp.com/nomad/0.2.3/nomad_0.2.3_linux_amd64.zip > /tmp/nomad.zip
-sudo unzip /tmp/nomad.zip -d /usr/bin
+unzip /tmp/nomad.zip -d /usr/bin
+rm /tmp/nomad.zip
 
 # Move the Nomad files.
-sudo mv /tmp/etc/nomad.d /etc/nomad.d
+mv /tmp/etc/nomad.d /etc/nomad.d
 
 # Set ownership and rights on Nomad directories.
-sudo install -o root -g root -m 755 -d /etc/nomad.d /var/local/nomad
-sudo install -o root -g root -m 644 /tmp/etc/systemd/system/nomad.service /etc/systemd/system/nomad.service
+install -o root -g root -m 755 -d /etc/nomad.d /var/local/nomad
+install -o root -g root -m 644 /tmp/etc/systemd/system/nomad.service /etc/systemd/system/nomad.service
 
-# Find the daemons.
-sudo systemctl daemon-reload
+# Enable the daemon.
+systemctl daemon-reload
+systemctl enable nomad
