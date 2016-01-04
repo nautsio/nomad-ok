b7843516329645880c08b97edd0c0ff7d491a360
Bastiaan Bakker <bbakker@xebia.com>
2016-01-04 23:05:51 +0100

    rename DNS zone resources to avoid conflict

--- gcloud.tf
+++ gcloud.tf
@@ -9,14 +9,14 @@ provider "google" {
 }
 
 resource "google_dns_managed_zone" "external" {
-    name = "external-zone"
-    description = "External zone"
+    name = "nomad-ok-external-zone"
+    description = "Nomad OK External zone"
     dns_name = "cloud.nauts.io."
 }
 
 resource "google_dns_managed_zone" "internal" {
-  name = "internal-zone"
-  description = "Internal zone"
+  name = "nomad-ok-internal-zone"
+  description = "Nomad OK Internal zone"
   dns_name = "int.nauts.io."
 }
 
