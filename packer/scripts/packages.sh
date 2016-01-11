95c32c1908ab44bbb884ae79de491f95f2f5de98
Bastiaan Bakker <bbakker@xebia.com>
2016-01-12 00:35:43 +0100

    install linux kernel headers for sysdig

--- packer/scripts/packages.sh
+++ packer/scripts/packages.sh
@@ -2,6 +2,10 @@
 # Quit on errors.
 set -e
 
-# Install tools.
 apt-get update
-apt-get install -y curl jq unzip
+
+# Install tools.
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl jq unzip linux-headers-$(uname -r)
+
+# Install kernel headers for Sysdig
+DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers-$(uname -r)
