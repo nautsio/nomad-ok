769c57f8bfb2a6b1688bcf39cf56d564daaa29f8
Bastiaan Bakker <bbakker@xebia.com>
2016-01-12 13:03:34 +0100

    switch back to standard instance type: small has issues with disk storage

--- variables.tf
+++ variables.tf
@@ -31,13 +31,15 @@ variable "nomad_client" {
     "groups" = 2
     "min_cluster_size" = 2
     "max_cluster_size" = 4
-#    "machine_type" = "n1-standard-1"
-    "machine_type" = "g1-small"
+    "machine_type" = "n1-standard-1"
+#    "machine_type" = "g1-small"
   }
 }
 
 variable "nomad_server" {
   default = {
+    "machine_type" = "n1-standard-1"
+#    "machine_type" = "g1-small"
     "cluster_size" = 3
   }
 }
