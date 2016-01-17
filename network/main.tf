fd24a75df45cbad539e757360205111b55eba9ec
Bastiaan Bakker <bbakker@xebia.com>
2016-01-17 23:01:19 +0100

    allow all internal networking plus ssh, nomad and consul from outside

--- network/main.tf
+++ network/main.tf
@@ -2,3 +2,45 @@ resource "google_compute_network" "network" {
   name = "${var.name}"
   ipv4_range = "${var.range}"
 }
+
+resource "google_compute_firewall" "allow-from-everywhere" {
+    name = "allow-from-everywhere"
+    network = "${google_compute_network.network.name}"
+
+    allow {
+        protocol = "icmp"
+    }
+
+    allow {
+        protocol = "tcp"
+        ports = ["22", "4646", "8500"]
+    }
+
+    allow {
+        protocol = "udp"
+        ports = ["8500"]
+    }
+
+    source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-all-internal" {
+    name = "${var.name}-allow-from-all"
+    network = "${google_compute_network.network.name}"
+
+    allow {
+        protocol = "icmp"
+    }
+
+    allow {
+        protocol = "tcp"
+        ports = ["0-65535"]
+    }
+
+    allow {
+        protocol = "udp"
+        ports = ["0-65535"]
+    }
+
+    source_ranges = ["${var.range}"]
+}
