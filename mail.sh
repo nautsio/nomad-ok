1515dc0093fd34f2b4a18d89cc81a458941e4074
Bastiaan Bakker <bbakker@xebia.com>
2016-01-23 02:07:03 +0100

    initial mail script

---
+++ mail.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+STACK=$1
+cat << EOF
+
+Dear $STACK,
+
+A personal training environment for the Scalable Container Scheduling Open Kitchen
+has been created for your pleasure.
+This mail contains the necessary hostname information and credentials to let you
+access your environment.
+All you need is an SSH client. Optionally a local Nomad binary can be used to query
+the Nomad cluster or submit jobs from your local machine.
+
+Your SSH private key is:
+$(cat stacks/$1/ssh-key)
+
+Save as 'ssh-key' and connect with:
+
+$ ssh -i ssh-key user@nomad-01.${STACK}.gce.nauts.io
+
+Alternatively the external ip addresses of all servers can be used, see server list:
+$(make list STACK=$STACK)
+
+User 'user' has privileges to run docker commands, and sudo as root if necessary.
+Try out 'nomad node-status' to see which servers are in the cluster.
+
+All server nodes also expose HTTP endpoints for Nomad and Consul, E.g.
+
+* http://nomad-01.$STACK.gce.nauts.io:4646/v1/nodes
+* http://nomad-01.$STACK.gce.nauts.io:8500/ui/
+
+NB. The environment will be destroyed after the Open Kitchen, so if you want to save
+your work do so before leaving.
+EOF
