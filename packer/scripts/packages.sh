14243d8ded50722dc00a4638f864a06e027a5d86
Bastiaan Bakker <bbakker@xebia.com>
2016-01-25 15:24:02 +0100

    install dnsutils to query Consul DNS.

--- packer/scripts/packages.sh
+++ packer/scripts/packages.sh
@@ -5,7 +5,7 @@ set -e
 apt-get update
 
 # Install tools.
-DEBIAN_FRONTEND=noninteractive apt-get install -y curl jq unzip linux-headers-$(uname -r)
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl jq unzip dnsutils
 
 # Install kernel headers for Sysdig
 DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers-$(uname -r)
