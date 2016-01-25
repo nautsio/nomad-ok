8c0fd0b469da6377294d4df83eff6804751ef49b
Bastiaan Bakker <bbakker@xebia.com>
2016-01-25 23:11:30 +0100

    create multipart mail with ssh-key as attachment

--- mail.sh
+++ mail.sh
@@ -3,24 +3,14 @@
 STACK=$1
 FROM=$2
 TO=$3
-cat << EOF
-From: <${FROM}>
-To: <${TO}>
-Subject: Nomad Open Kitchen training environment
-
+cat << EOF > stacks/${STACK}/message.txt
 Dear ${STACK},
 
-A personal training environment for the Scalable Container Scheduling Open Kitchen
-has been created for your pleasure.
-This mail contains the necessary hostname information and credentials to let you
-access your environment.
-All you need is an SSH client. Optionally a local Nomad binary can be used to query
-the Nomad cluster or submit jobs from your local machine.
+A personal training environment for the Scalable Container Scheduling Open Kitchen has been created for your pleasure.
+This mail contains the necessary hostname information and credentials to let you access your environment.
+All you need is an SSH client. Optionally a local Nomad binary can be used to query the Nomad cluster or submit jobs from your local machine.
 
-Your SSH private key is:
-$(cat stacks/$1/ssh-key)
-
-Save as 'ssh-key' and connect with:
+Your SSH private key is attached to this mail. Save as 'ssh-key' and connect with:
 
 $ ssh -i ssh-key user@nomad-01.${STACK}.gce.nauts.io
 
@@ -36,10 +26,28 @@ All server nodes also expose HTTP endpoints for Nomad and Consul, E.g.
 * http://nomad-01.$STACK.gce.nauts.io:4646/v1/nodes
 * http://nomad-01.$STACK.gce.nauts.io:8500/ui/
 
-NB. The environment will be destroyed after the Open Kitchen, so if you want to save
-your work do so before leaving.
+NB. The environment will be destroyed after the Open Kitchen, so if you want to save your work do so before leaving.
 
 Best regards,
 
 Erik Veld, Bastiaan Bakker
 EOF
+
+makemime -j                                                           \
+          \(                                                          \
+            -m "multipart/mixed"                                      \
+            -a "Mime-Version: 1.0"                                    \
+            -a "From: <${FROM}>"                                      \
+            -a "To: <${TO}>"                                          \
+            -a "Subject: Nomad Open Kitchen training environment"     \
+            \(                                                        \
+              -c "text/plain; charset=iso-8859-1"                     \
+              stacks/${STACK}/message.txt                             \
+            \)                                                        \
+          \)                                                          \
+          -o /dev/stdout                                              \
+          \(                                                          \
+            -c "application/x-pem-file"                               \
+            -a "Content-Disposition: attachment; filename=ssh-key"    \
+            stacks/${STACK}/ssh-key                                   \
+          \)
