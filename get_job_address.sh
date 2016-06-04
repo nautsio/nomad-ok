98be1cf8cd86485bed81e3fbb590c935a3afcd7c
Bastiaan Bakker <bbakker@xebia.com>
2016-06-04 09:42:42 +0200

    removed obsolete files

--- get_job_address.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-JOB=$1
-TASK=$2
-
-# This obviously only works if there is just one port exposed per task.
-for ALLOC in $(curl -s "$NOMAD_ADDR/v1/job/$JOB/allocations" | jq -r '.[] | select(.TaskStates.'"$TASK"'.State != "dead") | .ID'); do
-  curl -s "$NOMAD_ADDR/v1/allocation/$ALLOC" | jq -r '.TaskResources.'"$TASK"' | .Networks[0].IP + ":" + (.Networks[0].DynamicPorts[0].Value | tostring)'
-done
